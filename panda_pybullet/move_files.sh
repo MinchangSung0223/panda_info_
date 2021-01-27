ed6c0d7615d9d3fa0fd9d62600b146343570ad58
tjdalsckd <53217819+tjdalsckd@users.noreply.github.com>
2021-01-27 20:32:09 +0900

    Update move_files.sh

--- panda_pybullet/move_files.sh
+++ panda_pybullet/move_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+mkdir -p ../Hand-Eye-Calibration-Matlab/examples/cal0126_2/Images
+mkdir -p ../Hand-Eye-Calibration-Matlab/examples/cal0126_2/poses
+
 rm -r ../Hand-Eye-Calibration-Matlab/examples/cal0126_2/Images
 rm -r ../Hand-Eye-Calibration-Matlab/examples/cal0126_2/poses
 cp -r Images ../Hand-Eye-Calibration-Matlab/examples/cal0126_2/
 cp -r poses ../Hand-Eye-Calibration-Matlab/examples/cal0126_2/
-
